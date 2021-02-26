900496de00b253cad0974968e4eda7b9556c466b
Vasilii <porokh.vs@gmail.com>
2021-02-26 12:08:16 +0300

    Update webpackDev.js

--- webpackConfig/webpackDev.js
+++ webpackConfig/webpackDev.js
@@ -11,7 +11,6 @@ module.exports = () => {
             host: 'localhost',
             compress: true,
             port: 9000,
-            // allowedHosts: ['ci.ait']
         }
     });
     return config
